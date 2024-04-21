a293fab0624b95a358f5c10d9d269f5366ba5851
Raveena Ramesh Kale <110049768+raveenakale475@users.noreply.github.com>
2024-04-21 15:18:12 +0000

    operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "44hghg"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+// "33" = 33 
+// "33abc" = NaN
+//true = 1 false = 0
+//"" = false "rk" =true
+
+
+let someNum = 33
+
+let stringNum = String(someNum)
+
+console.log(typeof stringNum);
